9ca2641b22c8b3e3b9d71ceb41a43d11cdf6479a
Mikhail Bombin <boshyn@gmail.com>
2019-04-19 00:09:28 +0300

    2019-04-19 wrong test #8

--- src/Programms/Cinema.java
+++ src/Programms/Cinema.java
@@ -10,45 +10,36 @@ public class Cinema
     public static void programm()
     {
         List<String> inputList = new ArrayList<>();
-        byte result = 0;
+        long result = 0;
         InputData.VvodTexta.inputText(inputList);   
-        Integer [] sizeZal = Arrays.stream((inputList.get(0).split(" "))).map(x -> Integer.parseInt(x)).toArray(Integer[]::new);
-        Zal[] zal = new Zal[sizeZal[0]];
-        int kolMest = Integer.parseInt(inputList.get(inputList.size() - 1));
-        for (int i = 0; i < sizeZal[0]; i++) 
-        {
-            List<Integer> temp = Arrays.stream((inputList.get(i + 1).split(" ")))
-                    .map(x -> Integer.parseInt(x))
-                    .collect(Collectors.toList());
-            long free = temp.stream().filter(x -> x == 0).count();
-            if (free >= kolMest)
+        if (!inputList.isEmpty()){
+            Integer [] sizeZal = Arrays.stream((inputList.get(0).split(" "))).map(x -> Integer.parseInt(x)).toArray(Integer[]::new);
+            int kolMest = Integer.parseInt(inputList.get(inputList.size() - 1));        
+            for (int i = 0; i < sizeZal[0]; i++) 
             {
-                
-            }
-            System.out.println(free);
-            
-            zal[i] = new Zal(temp);
-        }   
-        zal[0].getRyad().stream().forEach(x -> System.out.print(x + " "));        
-        System.out.println();
-        zal[1].getRyad().stream().forEach(x -> System.out.print(x + " "));
-        System.out.println();
-        zal[2].getRyad().stream().forEach(x -> System.out.print(x + " "));
-        System.out.println();
-    }
-    
-    private static class Zal
-    {
-        List<Integer> ryad;
-
-        public Zal(List<Integer> ryad) 
-        {
-            this.ryad = ryad;
-        }     
-
-        public List<Integer> getRyad() {
-            return ryad;
+                List<Integer> temp = Arrays.stream((inputList.get(i + 1).split(" ")))
+                        .map(x -> Integer.parseInt(x))
+                        .collect(Collectors.toList());
+                long free = temp.stream().filter(x -> x == 0).count();
+                if (free >= kolMest)
+                {
+                    for (int j = 0; j < temp.size() - kolMest; j++) 
+                    {
+                        long tempLong = temp.stream().skip(j).limit(kolMest).filter(x -> x == 0).count();
+                        if (tempLong == kolMest)
+                        {
+                            result = tempLong;
+                            break;
+                        }                                        
+                    }                
+                }
+                if (result > 0)
+                {
+                    break;
+                }
+            }  
         }
-        
-    } 
+       
+        System.out.println(result);
+    }
 }
